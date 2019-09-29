9a1df47b67b442ab6320846271ebe3e16aef224b
shubhammittal05032000 <40912833+shubhammittal05032000@users.noreply.github.com>
2019-09-29 07:58:56 +0530

    Update Run.sh

--- Run.sh
+++ Run.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-#This script copies the files into the directories of the system to perform successful operation.
+
+#This script copies the files into the directories of the system to perform successful copy operation.
 
 #It also runs another script called "Requirement.sh" which contains the script to install the required packages.
 
