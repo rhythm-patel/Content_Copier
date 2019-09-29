7f7b12bc1f34366013616e7cbc4b6e543d811415
shubhammittal05032000 <40912833+shubhammittal05032000@users.noreply.github.com>
2019-09-29 07:57:32 +0530

    Update Run.sh

--- Run.sh
+++ Run.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
-cp check.py ~/.local/share/nautilus
-cp Copy_Contents.sh ~/.local/share/nautilus/scripts
-./Requirements.sh
+#This script copies the files into the directories of the system to perform successful operation.
 
+#It also runs another script called "Requirement.sh" which contains the script to install the required packages.
+
+cp check.py ~/.local/share/nautilus
+
+cp Copy_Contents.sh ~/.local/share/nautilus/scripts
+
+./Requirements.sh
