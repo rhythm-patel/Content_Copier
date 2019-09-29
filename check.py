e8e2171e1d5fab4a65fb326b958703c8f2207f54
shubhammittal05032000 <40912833+shubhammittal05032000@users.noreply.github.com>
2019-09-29 08:33:57 +0530

    Update check.py

--- check.py
+++ check.py
@@ -2,9 +2,14 @@ import pyperclip
 import os
 import sys
 
-filename = sys.argv[1]
+#pyperclip is a python library to copy the contents to clip board of OS.
 
-f=open(filename, "r")
-pyperclip.copy(f.read())
-pyperclip.paste()
+#filepath is a variable which stores the path of the file whose contents are to be copied.
 
+filepath = sys.argv[1]
+
+f=open(filepath, "r")
+
+pyperclip.copy(f.read()) #copy content of the file
+
+pyperclip.paste() #paste file content to clip board
